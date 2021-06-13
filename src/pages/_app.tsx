bf1a864590e431d4ad0246494af244aea5aecc8f
nsym <mickey-ya@outlook.jp>
2021-06-14 02:06:39 +0900

    update: 画像以外表示できるところまで

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { useEffect } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components'
+import {
+  ThemeProvider as MaterialUIThemeProvider,
+  StylesProvider
+} from '@material-ui/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+import theme from '../styles/theme'
+
+const MyApp = ({ Component, pageProps }: {Component: any, pageProps: any}): JSX.Element => {
+  // Remove the server-side injected CSS.(https://material-ui.com/guides/server-rendering/)
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles)
+    }
+  }, [])
+
+  return (
+    <StylesProvider injectFirst>
+      <MaterialUIThemeProvider theme={theme}>
+        <StyledComponentsThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </StyledComponentsThemeProvider>
+      </MaterialUIThemeProvider>
+    </StylesProvider>
+  )
 }
+
 export default MyApp
