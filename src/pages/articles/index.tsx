cc759fe6b08401001d0464b8f625377f5730a9b5
nishiyama <mickey-ya@outlook.jp>
2022-07-30 17:30:36 +0900

    add /articles menu

---
+++ src/pages/articles/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { GetStaticProps } from "next";
+import { getSortedArticlesData } from '../../lib/articles'
+import SEO from '../../components/SEO'
+import { ArticleRoot } from '../../components/ArticleRoot'
+import { ArticleHeader as IArticleHeader } from "../../models";
+
+type Props = {
+  articles: IArticleHeader[];
+};
+
+export default function Home({ articles }: Props): JSX.Element {
+  return (
+    <>
+      <SEO title="TOP" description="にしやまの技術ブログ" />
+      <ArticleRoot articles={articles} />
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const articles = await getSortedArticlesData();
+
+  return {
+    props: {
+      articles
+    },
+  };
+};
