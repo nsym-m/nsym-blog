77de62e4e2e39c7fa9203f5fcd58e7a48998f420
nishiyama <mickey-ya@outlook.jp>
2022-07-30 17:30:02 +0900

    delete sidemenu

--- src/components/ContentsLayout.tsx
+++ src/components/ContentsLayout.tsx
@@ -1,37 +1,15 @@
 import React from "react";
-import classnames from "classnames";
-import { AboutMe } from "./AboutMe";
 import styles from "./ContentsLayout.module.css";
 
 type Props = { sidemenu?: React.ReactNode };
 
 const ContentsLayout: React.FC<Props> = props => {
-  const [sidemenuOpen, setSidemenuOpen] = React.useState(false);
 
   const ref = React.useRef<HTMLDivElement>(null);
 
   return (
     <div ref={ref} className={styles.layout}>
       <div className={styles.contents}>{props.children}</div>
-      <div
-        className={classnames(
-          styles.sidemenuContainer,
-          sidemenuOpen && styles.sidemenuOpen,
-        )}>
-        <div className={styles.sidemenu}>
-          <div className={styles.aboutme}>
-            <AboutMe />
-          </div>
-        </div>
-      </div>
-      {/* <button
-        className={classnames(
-          styles.sidemenuOpenButton,
-          isInViewport && styles.showSidemenuOpenButton,
-        )}
-        onClick={() => setSidemenuOpen(prev => !prev)}>
-        <BarsIcon className={styles.menuIcon} />
-      </button> */}
     </div>
   );
 };
