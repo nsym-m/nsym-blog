8598867b4651d6070eb97b337da69318b0117dcd
nishiyama <nishiyama_mikiya@andfactory.co.jp>
2024-05-02 06:01:44 +0900

    add: ダークモード導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { GA_TRACKING_ID } from "../lib/gtag";
 import { ReactNode } from 'react'
+import { ThemeProvider } from "../components/Theme/ThemeProvider";
 
 export default function RootLayout({
   children,
@@ -7,8 +8,12 @@ export default function RootLayout({
   children: ReactNode
 }): JSX.Element {
   return (
-    <html lang="ja-JP">
-      <body>{children}</body>
+    <html lang="ja-JP" suppressHydrationWarning>
+      <body>
+        <ThemeProvider>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import SEO from '../components/SEO'
 import { AboutMe } from '../components/AboutMe/AboutMe';
 import ContentsLayout from '../components/ContentsLayout/ContentsLayout';
+import { ThemeToggle } from "../components/Theme/ThemeToggle";
 
 export default function Home(): JSX.Element {
   return (
     <>
       <SEO title="プロフィール" description="にしやまの技術ブログ" />
+      <ThemeToggle />
       <ContentsLayout>
         <AboutMe />
       </ContentsLayout>
