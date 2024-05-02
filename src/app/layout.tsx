88e4890550083c3ef3db8dba77b6932630568942
nishiyama <nishiyama_mikiya@andfactory.co.jp>
2024-05-03 06:38:55 +0900

    add: gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,23 @@ export default function RootLayout({
     <html lang="ja-JP" suppressHydrationWarning>
       <head>
         <meta name="color-scheme" content="light dark" />
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </head>
       <body>
         <ThemeProvider>
